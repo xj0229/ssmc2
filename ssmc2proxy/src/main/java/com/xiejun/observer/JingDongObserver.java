ea9f8ba4287e23813d8b925da9bcbfa122bc03e3
xiejun <xiejun@xiejunvm1>
2019-04-21 14:20:31 +0800

    vv3

--- ssmc2proxy/src/main/java/com/xiejun/observer/JingDongObserver.java
+++ ssmc2proxy/src/main/java/com/xiejun/observer/JingDongObserver.java
@@ -3,7 +3,7 @@ package com.xiejun.observer;
 import java.util.Observable;
 import java.util.Observer;
 
-public class JingDongObserver  implements Observer {
+public class JingDongObserver implements Observer {
     @Override
     public void update(Observable o, Object product) {
         String newProduct = (String) product;
